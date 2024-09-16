ec596010f9c5c025446fa97e9b884840f0c48bc7
vMattS <mate.sepulveda@duocuc.cl>
2024-09-15 20:18:46 -0400

    not found

--- src/app/pages/notfound/notfound-routing.module.ts
+++ src/app/pages/notfound/notfound-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { NotfoundPage } from './notfound.page';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundPage } from './notfound.page'; // Usa el nombre correcto
 
 const routes: Routes = [
   {
@@ -12,6 +11,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class NotfoundPageRoutingModule {}
--- src/app/pages/notfound/notfound.page.ts
+++ src/app/pages/notfound/notfound.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.page.html',
   styleUrls: ['./notfound.page.scss'],
 })
-export class NotfoundPage implements OnInit {
+export class NotfoundPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController) {}
 
   ngOnInit() {
+    // Deshabilitar el menú lateral cuando se accede a la página de error 404
+    this.menuCtrl.enable(false);
   }
 
+  ngOnDestroy() {
+    // Habilitar el menú lateral cuando se abandona la página de error 404
+    this.menuCtrl.enable(true);
+  }
 }
