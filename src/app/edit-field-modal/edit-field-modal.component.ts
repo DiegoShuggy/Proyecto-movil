6c354cadfc50c7d283644c043697da91a9f8dac7
Diego Julio Pinto Beltran <die.pintob@duocuc.cl>
2024-09-20 18:59:49 -0300

    Avance
    Diseño/Formularios/reestructuracion de sccs para funcionamiento de diseño de botones cards modals etc

---
+++ src/app/edit-field-modal/edit-field-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-field-modal',
+  templateUrl: './edit-field-modal.component.html',
+  styleUrls: ['./edit-field-modal.component.scss'],
+})
+export class EditFieldModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
