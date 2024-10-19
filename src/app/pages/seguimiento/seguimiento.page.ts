07ca9a1a627d6cc8cd1c2d7b1879c89ecc47fa40
vMattS <mate.sepulveda@duocuc.cl>
2024-10-19 13:13:02 -0300

    seguimiento

--- src/app/pages/seguimiento/seguimiento.page.ts
+++ src/app/pages/seguimiento/seguimiento.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
 
 @Component({
   selector: 'app-seguimiento',
   templateUrl: './seguimiento.page.html',
   styleUrls: ['./seguimiento.page.scss'],
 })
-export class SeguimientoPage implements OnInit {
+export class SeguimientoPage {
+  @ViewChild('map', { static: false }) mapRef!: ElementRef;
 
-  constructor() { }
+  map!: GoogleMap;  // Usamos '!' para indicar que será inicializada después
 
-  ngOnInit() {
+  constructor() {}
+
+  async ngAfterViewInit() {
+    this.loadMap();
   }
 
+  async loadMap() {
+    this.map = await GoogleMap.create({
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: 'AIzaSyDw1-dlE0P_kOEmrVCsWVMqerXEG2sKQWk',
+      config: {
+        center: {
+          lat: -33.45694, // Latitud inicial (ejemplo: Santiago, Chile)
+          lng: -70.64827, // Longitud inicial
+        },
+        zoom: 15, // Nivel de zoom inicial
+      },
+    });
+
+    // Si quieres agregar un marcador en el mapa
+    await this.map.addMarker({
+      coordinate: {
+        lat: -33.45694,
+        lng: -70.64827,
+      },
+      title: 'Tu ubicación',
+    });
+  }
 }
